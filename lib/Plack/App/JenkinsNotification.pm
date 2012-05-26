d12fcc9005538410daf173ab04d13c1bb59d7e62
Yo-An Lin (林佑安) <cornelius@cpan.org>
2012-05-26 03:46:05 +0000

    Import of CORNELIUS/Plack-App-JenkinsNotification-0.01 from CPAN.
    gitpan-cpan-distribution: Plack-App-JenkinsNotification
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         CORNELIUS/Plack-App-JenkinsNotification-0.01.tar.gz
    gitpan-cpan-author:       CORNELIUS
    gitpan-cpan-maturity:     released

---
+++ lib/Plack/App/JenkinsNotification.pm
@@ -0,0 +1,63 @@
+package Plack::App::JenkinsNotification;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+use parent qw/Plack::Component/;
+use Plack::Util;
+use Plack::MIME;
+use Plack::Util::Accessor qw(on_notify);
+use Plack::Response;
+use Plack::Request;
+use Net::Jenkins;
+use Jenkins::NotificationListener;
+
+sub call { 
+    my ($self,$env) = @_;
+    my $req = Plack::Request->new($env);
+    my $body = $req->raw_body;
+
+    my $notification = parse_jenkins_notification $body;
+
+    if( $self->on_notify ) {
+        $self->on_notify->( $env, $notification );
+    }
+
+    my $response = Plack::Response->new(200);
+    $response->body("success");
+    return $response->finalize;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::JenkinsNotification -
+
+=head1 SYNOPSIS
+
+    use Plack::App::JenkinsNotification;
+
+    builder {
+        mount "/jenkins" => Plack::App::JenkinsNotification->new({ on_notify => sub {
+            
+            
+        }});
+    };
+
+=head1 DESCRIPTION
+
+Plack::App::JenkinsNotification is
+
+=head1 AUTHOR
+
+Yo-An Lin E<lt>cornelius.howl {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
